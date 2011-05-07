ae5c907227c8026f7da7473d1a2468756e2fbb5d
OZAWA Sakuro <sakuro@2238club.org>
2011-05-07 17:02:54 +0900

    Cope with deprecations in rubygems 1.8.x.

--- lib/rubygems/commands/orphan_command.rb
+++ lib/rubygems/commands/orphan_command.rb
@@ -8,8 +8,12 @@ class Gem::Commands::OrphanCommand < Gem::Command
   end
 
   def execute
-    index = Gem::SourceIndex.from_installed_gems
-    specs = index.map {|name, spec| spec }
+    if Gem::Specification.respond_to?(:all)
+      specs = Gem::Specification.all
+    else
+      index = Gem::SourceIndex.from_installed_gems
+      specs = index.map {|name, spec| spec }
+    end
     dependencies = specs.map(&:dependencies).flatten.uniq
     runtime_dependencies = dependencies.select {|dep| dep.type == :runtime }
 
